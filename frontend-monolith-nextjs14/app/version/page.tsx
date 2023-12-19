9933cab3056badd9bff3b49dca406606e219d794
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-12-19 22:58:35 +0200

    fix: for build

--- frontend-monolith-nextjs14/app/version/page.tsx
+++ frontend-monolith-nextjs14/app/version/page.tsx
@@ -1,16 +1,7 @@
-import WYSIWYG from "@/components/editorjs/editorjs";
-import dynamic from "next/dynamic";
-
-const Editor = dynamic(() => import("@editorjs/editorjs"), {
-  ssr: false,
-  loading: <p>loading...</p>,
-});
-
 const VersionPage = () => {
   return (
     <main>
       <h1>Версия приложения</h1>
-      <WYSIWYG />
     </main>
   );
 };
